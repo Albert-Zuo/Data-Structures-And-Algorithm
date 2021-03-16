2d9494b9456ba80c7dd8f9b2dc09ef54b1dcc9b5
Albert <617899358@qq.com>
2021-03-16 14:27:49 +0800

    update arrayStack

--- structures/stack/arraystack.go
+++ structures/stack/arraystack.go
@@ -3,22 +3,19 @@ package stack
 import (
 	"bytes"
 	"fmt"
-	"github.com/Albert-Zuo/Data-Structures-And-Algorithm/structures/array"
 )
 
 // 线性表版本实现 栈
 type ArrayStack struct {
-	list array.List
+	data []interface{}
 }
 
 func NewArrayStack() *ArrayStack {
-	return &ArrayStack{
-		list: array.NewArray(),
-	}
+	return &ArrayStack{make([]interface{}, 0)}
 }
 
 func (stack *ArrayStack) GetSize() int {
-	return stack.list.Size()
+	return len(stack.data)
 }
 
 func (stack *ArrayStack) IsEmpty() bool {
@@ -26,23 +23,24 @@ func (stack *ArrayStack) IsEmpty() bool {
 }
 
 func (stack *ArrayStack) Push(e interface{}) {
-	stack.list.Append(e)
+	stack.data = append(stack.data, e)
 }
 
 func (stack *ArrayStack) Pop() interface{} {
 	if stack.IsEmpty() {
 		return nil
 	}
-	return stack.list.Remove(stack.GetSize() - 1)
+	idx := len(stack.data)-1
+	e := stack.data[idx]
+	stack.data = stack.data[:idx]
+	return e
 }
 
 func (stack *ArrayStack) Peek() interface{} {
 	if stack.IsEmpty() {
 		return nil
 	}
-	e, _ := stack.list.Get(stack.GetSize() - 1)
-	return e
-
+	return stack.data[len(stack.data)-1]
 }
 
 func (stack *ArrayStack) String() string {
@@ -51,7 +49,7 @@ func (stack *ArrayStack) String() string {
 	buffer.WriteString("Stack: ")
 	buffer.WriteString("[")
 	for i := 0; i < stack.GetSize(); i++ {
-		buffer.WriteString(fmt.Sprint(stack.list.Get(i)))
+		buffer.WriteString(fmt.Sprint(stack.data[i]))
 		if i != stack.GetSize()-1 {
 			buffer.WriteString(", ")
 		}
